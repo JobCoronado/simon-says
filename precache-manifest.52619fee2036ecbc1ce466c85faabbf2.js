ddd66e9e41d335db5b406ad0729299ec4f0efbef
JobCoronado <46569370+JobCoronado@users.noreply.github.com>
2019-04-05 22:12:52 -0500

    Add files via upload

---
+++ precache-manifest.52619fee2036ecbc1ce466c85faabbf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f98a5b294e5036f9fc0",
+    "url": "/static/css/main.8e070486.chunk.css"
+  },
+  {
+    "revision": "4f98a5b294e5036f9fc0",
+    "url": "/static/js/main.9419fd6c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "558ecd21be083ededbd6",
+    "url": "/static/css/2.99e76bb2.chunk.css"
+  },
+  {
+    "revision": "558ecd21be083ededbd6",
+    "url": "/static/js/2.a6bbc099.chunk.js"
+  },
+  {
+    "revision": "36b1b47d506c571c1cf33b38337712e0",
+    "url": "/static/media/pitido.36b1b47d.mp3"
+  },
+  {
+    "revision": "869809b754acc5563210129028f498ad",
+    "url": "/index.html"
+  }
+];
